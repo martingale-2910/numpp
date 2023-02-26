a19cdc3f2235b626d3c6dd258e9a2bcb7e8a9259
martingale <124511455+martingale-2910@users.noreply.github.com>
2023-02-26 17:54:32 +0100

    Add base version of vector implementation

--- src/main.cxx
+++ src/main.cxx
@@ -3,14 +3,23 @@
 
 int main(int argc, char** argv)
 {
-    auto rvd = rvecd(std::valarray<double>{0.0, 1.0, 2.0, 3.0, 4.0});
-    auto cvd = cvecd(std::valarray<double>{0.0, 1.0, 2.0, 3.0, 4.0});
-    auto rvb = rvecb(std::valarray<bool>{false, true, true, true});
+    auto rvd = rvecd(std::valarray<double>{0.0, 1.1, 2.2, 3.3, 4.4});
+    auto cvd = cvecd(std::valarray<double>{0.0, 1.1, 2.2, 3.3, 4.4});
+    auto rvb = rvd > 1.0;
+    std::cout << rvb << std::endl;
+    std::cout << rvd[rvb] << std::endl;
+    std::cout << rvd.transpose() << std::endl;
     std::cout << ((((2.0*rvd)/2.0)+1)-1) << std::endl;
     std::cout << 3.0/rvd << std::endl;
     std::cout << rvd/2.0 << std::endl;
     std::cout << cvd+cvd << std::endl;
     std::cout << rvd*cvd << std::endl;
-    std::cout << rvd[rvb] << std::endl;
+    std::cout << abs(rvd) << std::endl;
+    std::cout << pow(rvd, 2) << std::endl;
+    std::cout << sqrt(rvd) << std::endl;
+    std::cout << exp(rvd) << std::endl;
+    std::cout << log(rvd) << std::endl;
+    std::cout << ceil(rvd) << std::endl;
+    std::cout << floor(rvd) << std::endl;
     return 0;
 }   
--- src/vec.hxx
+++ src/vec.hxx
@@ -2,12 +2,12 @@
 #define VEC_HXX
 
 #include <iostream> // std::cout, std::endl, std::ostream
-#include <iomanip> // ?
 #include <type_traits> // std::enable_if, std::is_arithmetic, std::conjunction_v
 #include <valarray> // std::valarray
 #include <cassert> // std::assert
 #include <string> // std::to_string
 #include <numeric> // std::accumulate
+#include <cmath> // std::pow, std::exp, std::sqrt, std::ceil,. std::floor
 
 enum struct Orientation { ROW, COL };
 
@@ -26,7 +26,7 @@ struct vec
     
     arithmetic_t operator[](std::size_t i) const { return (this->data)[i]; }
     arithmetic_t& operator[](std::size_t i) { return (this->data)[i]; }
-    vec operator[](vec<o, bool> const & b) const { return vec<o, arithmetic_t>((this->data)[b.data]); }
+    vec<o, arithmetic_t> operator[](vec<o, bool> const & b) const { return vec<o, arithmetic_t>((this->data)[b.data]); }
     arithmetic_t max() const { return (this->data).max(); }
     arithmetic_t min() const { return (this->data).min(); }
 };
@@ -44,6 +44,8 @@ using cvecb = cvec<bool>;
 
 template<template<typename> typename predicate, typename... ts>
 using enable_if_all = std::enable_if_t<std::conjunction_v<predicate<ts>...>, bool>;
+
+// Basic arithmetic operations
 
 template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
 vec<o, arithmetic_t1> operator+(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, arithmetic_t1>((v.data) + d); }
@@ -64,6 +66,7 @@ vec<o, arithmetic_t1> operator*(vec<o, arithmetic_t1> const & v, arithmetic_t2 d
 template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
 vec<o, arithmetic_t1> operator*(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return (v*d); }
 
+// Hadamard product
 template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
 vec<o, arithmetic_t1> operator*(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
 {
@@ -71,6 +74,7 @@ vec<o, arithmetic_t1> operator*(vec<o, arithmetic_t1> const & v1, vec<o, arithme
     return vec<o, arithmetic_t1>(v1.data*v2.data);
 }
 
+// Dot product
 template<typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
 arithmetic_t1 operator*(rvec<arithmetic_t1> const & rv, cvec<arithmetic_t2> const & cv)
 {
@@ -104,30 +108,136 @@ vec<o, arithmetic_t1> operator/(vec<o, arithmetic_t1> const & v1, vec<o, arithme
     return vec<o, arithmetic_t1>(v1.data/v2.data);
 }
 
+// Boolean operators
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator>(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, bool>(v.data > d); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator>(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return vec<o, bool>(d > v.data); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator>(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
+{
+    assert(v1.size() == v2.size());
+    return vec<o, bool>(v1.data > v2.data);
+}
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator>=(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, bool>(v.data >= d); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator>=(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return vec<o, bool>(d >= v.data); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator>=(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
+{
+    assert(v1.size() == v2.size());
+    return vec<o, bool>(v1.data >= v2.data);
+}
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator<(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, bool>(v.data < d); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator<(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return vec<o, bool>(d < v.data); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator<(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
+{
+    assert(v1.size() == v2.size());
+    return vec<o, bool>(v1.data < v2.data);
+}
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator<=(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, bool>(v.data <= d); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator<=(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return vec<o, bool>(d <= v.data); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator<=(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
+{
+    assert(v1.size() == v2.size());
+    return vec<o, bool>(v1.data <= v2.data);
+}
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator==(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, bool>(v.data == d); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator==(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return vec<o, bool>(d == v.data); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator==(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
+{
+    assert(v1.size() == v2.size());
+    return vec<o, bool>(v1.data == v2.data);
+}
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator!=(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, bool>(v.data != d); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator!=(arithmetic_t2 d, vec<o, arithmetic_t1> const & v) { return vec<o, bool>(d != v.data); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, bool> operator!=(vec<o, arithmetic_t1> const & v1, vec<o, arithmetic_t2> const & v2)
+{
+    assert(v1.size() == v2.size());
+    return vec<o, bool>(v1.data != v2.data);
+}
+
+// Stream operations
+
 template<Orientation o, typename arithmetic_t, enable_if_all<std::is_arithmetic, arithmetic_t> = true>
 std::ostream& operator<<(std::ostream& os, vec<o, arithmetic_t> const & vec)
 {
-    if (vec.orientation == Orientation::ROW)
+    auto acc_fn = [orientation=vec.orientation](std::string &acc, arithmetic_t const &elem)
     {
-        std::string joined_inputs = std::accumulate(
+        if (orientation == Orientation::ROW)
+        {
+            return acc.empty() ? std::to_string(elem) : acc + ", " + std::to_string(elem);
+        }
+        else
+        {
+            return acc.empty() ? "|" + std::to_string(elem) + ",|" : acc + "\n" + "|" + std::to_string(elem) + ",|";
+        }
+    };
+
+    std::string joined_inputs = std::accumulate(
             std::begin(vec.data), 
             std::end(vec.data), 
             std::string(), 
-            [](std::string &acc, arithmetic_t const &elem) { return acc.empty() ? std::to_string(elem) : acc + ", " + std::to_string(elem); }
-        );
-        os << "[" << joined_inputs << "]";
-    }
-    else
-    {
-        std::string joined_inputs = std::accumulate(
-            std::begin(vec.data), 
-            std::end(vec.data), 
-            std::string(), 
-            [](std::string &acc, arithmetic_t const &elem) { return acc.empty() ? "|" + std::to_string(elem) + ";|" : acc + "\n" + "|" + std::to_string(elem) + ";|"; }
-        );
-        os << joined_inputs;
-    }
+            acc_fn
+    );
+
+    vec.orientation == Orientation::ROW ? os << "[" << joined_inputs << "]" : os << joined_inputs;
+
     return os;
 }
+
+// Math overloards
+
+template<Orientation o, typename arithmetic_t1, enable_if_all<std::is_arithmetic, arithmetic_t1> = true>
+vec<o, arithmetic_t1> abs(vec<o, arithmetic_t1> const & v) { return vec<o, arithmetic_t1>(std::abs(v.data)); }
+
+template<Orientation o, typename arithmetic_t1, typename arithmetic_t2, enable_if_all<std::is_arithmetic, arithmetic_t1, arithmetic_t2> = true>
+vec<o, arithmetic_t1> pow(vec<o, arithmetic_t1> const & v, arithmetic_t2 d) { return vec<o, arithmetic_t1>(std::pow(v.data, d)); }
+
+template<Orientation o, typename arithmetic_t1, enable_if_all<std::is_arithmetic, arithmetic_t1> = true>
+vec<o, arithmetic_t1> sqrt(vec<o, arithmetic_t1> const & v) { return vec<o, arithmetic_t1>(std::sqrt(v.data)); }
+
+template<Orientation o, typename arithmetic_t1, enable_if_all<std::is_arithmetic, arithmetic_t1> = true>
+vec<o, arithmetic_t1> exp(vec<o, arithmetic_t1> const & v) { return vec<o, arithmetic_t1>(std::exp(v.data)); }
+
+template<Orientation o, typename arithmetic_t1, enable_if_all<std::is_arithmetic, arithmetic_t1> = true>
+vec<o, arithmetic_t1> log(vec<o, arithmetic_t1> const & v) { return vec<o, arithmetic_t1>(std::log(v.data)); }
+
+template<Orientation o, typename arithmetic_t1, enable_if_all<std::is_arithmetic, arithmetic_t1> = true>
+vec<o, arithmetic_t1> ceil(vec<o, arithmetic_t1> const & v) { return vec<o, arithmetic_t1>(v.data.apply(std::ceil)); }
+
+template<Orientation o, typename arithmetic_t1, enable_if_all<std::is_arithmetic, arithmetic_t1> = true>
+vec<o, arithmetic_t1> floor(vec<o, arithmetic_t1> const & v) { return vec<o, arithmetic_t1>(v.data.apply(std::floor)); }
 
 #endif // VEC_HXX
